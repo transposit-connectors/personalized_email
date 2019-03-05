1de968b90596431b556d26fa9a7130f5720f89be
Nina Yang <nina@transposit.com>
2019-03-05 21:08:30 +0000

    New google mail params

--- sendEmail.js
+++ sendEmail.js
@@ -1,7 +1,8 @@
 function run(params) {
   return api.run("google_mail.send_message", {
     to: params.email,
-    messageHtml: params.body,
+    message: params.body,
+    contentType: "text/html",
     subject: params.subject,
     userId: "me"
   });
